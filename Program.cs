80df2ee0851e040431285ee7d4172f7367abd16d
omartrese <108763869+omartrese@users.noreply.github.com>
2022-12-19 19:42:43 +0100

    completed

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int counter = 5;
+
+int a = 0; 
+
+int b = 1;
+
+int fibonacci(int before, int after, int counter)
+{
+    if(counter <= 0) return after;
+    
+
+    return fibonacci(after, before + after, counter - 1);
+    
+}
+
+Console.WriteLine(fibonacci(a, b, counter));
